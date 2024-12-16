56532fa7c1f117c20ac7f06c80a8cd3e94d289d1
Michael Rojas <81451650+fabianrojasSab@users.noreply.github.com>
2024-12-16 01:07:49 +0000

    feat

--- script.sh
+++ script.sh
@@ -37,3 +37,73 @@ md c:\mongodb\mongos
     start mongod --bind_ip localhost --port 27041 --dbpath c:\mongodb\config\config1 --replSet configReplSet --configsvr
     start mongod --bind_ip localhost --port 27042 --dbpath c:\mongodb\config\config2 --replSet configReplSet --configsvr
     start mongod --bind_ip localhost --port 27043 --dbpath c:\mongodb\config\config3 --replSet configReplSet --configsvr
+
+# 4. Iniciar las configuraciones de replicación
+# Conéctate a cada conjunto de réplicas y configúralos:
+    # Config servers:
+        mongosh --port 27041
+        rs.initiate({
+            _id: "configReplSet",
+            configsvr: true,
+            members: [
+                { _id: 0, host: "localhost:27041" },
+                { _id: 1, host: "localhost:27042" },
+                { _id: 2, host: "localhost:27043" }
+            ]
+        })
+
+# Shard 1:
+    mongosh --port 27011
+    rs.initiate({
+        _id: "shard1",
+        members: [
+            { _id: 0, host: "localhost:27011" },  
+        ]
+    })
+    rs.add("localhost:27012"); // Nodo secundario
+    rs.add("localhost:27013"); // Nodo secundario
+
+
+# Shard 2:
+
+    mongosh --port 27021
+    rs.initiate({
+        _id: "shard2",
+        members: [
+            { _id: 0, host: "localhost:27021" },
+        ]
+    })
+    rs.add("localhost:27022"); // Nodo secundario
+    rs.add("localhost:27023"); // Nodo secundario
+
+# Shard 3:
+    mongosh --port 27031
+    rs.initiate({
+        _id: "shard3",
+        members: [
+            { _id: 0, host: "localhost:27031" },
+        ]
+    })
+    rs.add("localhost:27032"); // Nodo secundario
+    rs.add("localhost:27033"); // Nodo secundario
+
+# 5. Iniciar el mongos router
+# El mongos se utiliza como punto de entrada al clúster. Inicia el mongos y conéctalo a los config servers:
+    start mongos --configdb configReplSet/localhost:27041,localhost:27042,localhost:27043 --bind_ip localhost --port 27050
+
+
+# 6. Configurar el Sharding
+# Conéctate al mongos para configurar los shards:
+
+    mongosh --port 27050
+
+    #Agregar shards al clúster
+    sh.addShard("shard1/localhost:27011,localhost:27012,localhost:27013");
+    sh.addShard("shard2/localhost:27021,localhost:27022,localhost:27023");
+    sh.addShard("shard3/localhost:27031,localhost:27032,localhost:27033");
+
+
+# 7. Habilitar sharding para la base de datos
+# Habilita el sharding para la base de datos evento_deportivo:
+
+    sh.enableSharding("evento_deportivo");
